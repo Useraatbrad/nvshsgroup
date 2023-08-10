003f207faca8ad42c16efdabe655bb5a3f3f38f7
Useraatbrad <139950347+Useraatbrad@users.noreply.github.com>
2023-08-11 09:13:47 +1000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyB5suKK2tX_IhLsewYJaPm0lNqbKNcw3ew",
+  authDomain: "noidas-94ef7.firebaseapp.com",
+  projectId: "noidas-94ef7",
+  storageBucket: "noidas-94ef7.appspot.com",
+  messagingSenderId: "545818177015",
+  appId: "1:545818177015:web:183ad2a60f9873c03312f2",
+};
+
+firebase.initializeApp(firebaseConfig);
