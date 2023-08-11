46b250f485f1ad51fe8211d98e43efa21802cbf3
Useraatbrad <139950347+Useraatbrad@users.noreply.github.com>
2023-08-11 11:46:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const db = firebase.firestore();
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
 
 // Send message to group chat
-document.getElementById('send-button').addEventListener('click', () => {
-  const messageInput = document.getElementById('message-input');
+sendButton.addEventListener('click', sendMessage);
+
+// Handle "Enter" key press
+messageInput.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    event.preventDefault(); // Prevent line break in the textarea
+    sendMessage();
+  }
+});
+
+function sendMessage() {
   const message = messageInput.value;
 
   if (message.trim() !== '') {
-    // Add message to Firestore
+    // Add message to Firestore with sender's user ID (assuming you have authentication)
     db.collection('groupChat').add({
       text: message,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      userId: firebase.auth().currentUser.uid // Replace with actual user ID
     });
 
     messageInput.value = '';
   }
-});
+}
 
 // Display messages in real-time
 const messageContainer = document.getElementById('message-container');
@@ -25,6 +37,12 @@ db.collection('groupChat').orderBy('timestamp').onSnapshot(snapshot => {
     const messageElement = document.createElement('div');
     messageElement.classList.add('message');
     messageElement.textContent = messageData.text;
+    
+    // Add a class to differentiate between messages sent by the current user and others
+    if (messageData.userId === firebase.auth().currentUser.uid) {
+      messageElement.classList.add('my-message');
+    }
+    
     messageContainer.appendChild(messageElement);
   });
 });
